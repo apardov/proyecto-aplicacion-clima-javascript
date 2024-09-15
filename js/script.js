9456c06c742c880a55ae004ae422127f48f25af2
Andres Pardo <and.pardo.vergara@gmail.com>
2024-09-14 21:43:24 -0300

    proyecto de aplicación de clima que se conecta a api weather

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '3ffe47ee73f560d3c03f517cbbf98f3d';
+const diffKelvin = 273.15;
+
+
+document.getElementById('searchButton').addEventListener('click', () => {
+	const city = document.getElementById('cityInput').value;
+	if (city) {
+		fetchWeather(city)
+	} else {
+		alert('Ingrese una ciudad valida');
+	}
+})
+
+const fetchWeather = (city) => {
+	fetch(`${urlBase}?q=${city}&appid=${apiKey}&lang=es`)
+		.then(data => data.json())
+		.then(data => showCity(data))
+}
+
+const showCity = (data) => {
+	const responseData = document.getElementById('responseData');
+	responseData.innerHTML = `
+    <div class="weather-info">
+        <h2>${data.name}</h2>
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="weather">
+        <p>${data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1).toLowerCase()}</p>
+        <p><strong>La temperatura es de : </strong>${Math.round(data.main.temp - 273)} °C</p>
+    </div>
+`;
+}
